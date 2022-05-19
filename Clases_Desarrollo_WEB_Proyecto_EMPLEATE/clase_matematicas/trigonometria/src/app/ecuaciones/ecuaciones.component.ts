817b9ec5fffbf2e1e201383e4a1a577ca501289f
Elías <delias2798@hotmail.com>
2022-05-18 21:14:39 -0600

    first commit

---
+++ Clases_Desarrollo_WEB_Proyecto_EMPLEATE/clase_matematicas/trigonometria/src/app/ecuaciones/ecuaciones.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ecuaciones',
+  templateUrl: './ecuaciones.component.html',
+  styleUrls: ['./ecuaciones.component.css']
+})
+export class EcuacionesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
